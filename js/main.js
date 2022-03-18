d6b89f1b839756eae28abf8a0ecd4daf2748f241
SERGEYBOBKOV <sergey_bobkov@inbox.ru>
2022-03-18 17:13:42 +0300

    dynamic and imutable flow

--- js/main.js
+++ js/main.js
@@ -1,19 +1,49 @@
-const state = {
-    time: new Date(),
-    lots: [
-        {
-            id: 1,
-            name: 'Apple',
-            description: 'Apple description',
-            price: 16
-        },
-        {
-            id: 2,
-            name: 'Orange',
-            description: 'Orange description',
-            price: 41
+const api = {
+    get(url) {
+        switch(url) {
+            case '/lots':
+                return new Promise((resolve) => {
+                    setTimeout(()=> {
+                        resolve([
+                            {
+                                id: 1,
+                                name: 'Apple',
+                                description: 'Apple description',
+                                price: 16
+                            },
+                            {
+                                id: 2,
+                                name: 'Orange',
+                                description: 'Orange description',
+                                price: 41
+                            }
+                        ])
+                    }, 1000)
+                })
+            default:
+                throw new Error('Unknown address')
         }
-    ]
+    }
+}
+
+const stream = {
+    subscribe(channel, listener) {
+        const match = /price-(\d+)/.exec(channel);
+        if (match) {
+            setInterval(()=> {
+                listener({
+                    id: parseInt(match[1]),
+                    price: Math.round((Math.random()*10+30))
+                })
+            }, 400)
+        }
+    }
+}
+
+
+let state = {
+    time: new Date(),
+    lots: null
 }
 
 function App( { state } )
@@ -47,16 +77,43 @@ function Logo() {
 
 function Clock({ time })
 {
-    const clock = document.createElement('div');
-    clock.className = 'clock';
-    clock.innerText = time.toLocaleTimeString();
-    return clock;
+    const node = document.createElement('div');
+    node.className = 'clock';
+   
+    const value = document.createElement('span');
+    value.className = 'value';
+    
+    value.innerText = time.toLocaleTimeString();
+    
+    node.append(value);
+    
+    const icon = document.createElement('span');
+    
+    if(time.getHours() >= 7 && time.getHours() <= 21) {
+        icon.className = 'icon day';
+    } else {
+        icon.className = 'icon night'
+    }
+    
+    node.append(icon);
+    
+    return node;
+}
+
+function Loading() {
+    const node = document.createElement('div');
+    node.className = 'loading';
+    node.innerText = '...loading';
+    return node;
 }
 
 
-
-
 function Lots({ lots }) {
+    
+    if(lots === null) {
+        return Loading();
+    }
+    
     const list = document.createElement('div');
     list.className = 'lots';
     
@@ -89,13 +146,58 @@ function Lot({lot}) {
 }
 
 function render(newDom, realDomRoot) {
+    realDomRoot.innerHTML = '';
     realDomRoot.append(newDom);
 }
 
-render(
-    App( { state } ),
-    document.getElementById('root')
-);
+function renderView(state) {
+    render(
+        App( { state } ),
+        document.getElementById('root')
+    );
+}
+
+renderView(state);
+
+setInterval(()=> {
+    
+    state = {
+        ...state,
+        time: new Date()
+    };
+    
+    renderView(state);
+    
+}, 1000);
+
+api.get('/lots').then((lots) => {
+    state = {
+        ...state,
+        lots
+    }
+    renderView(state);
+    const onPrice = (data) => {
+        state = {
+            ...state,
+            lots: state.lots.map((lot) => {
+                if (lot.id === data.id) {
+                    return {
+                        ...lot,
+                        price: data.price
+                    }
+                }
+                return lot;
+            })
+        }
+        renderView(state);
+    }
+    
+    lots.forEach((lot)=> {
+        stream.subscribe(`price-${lot.id}`, onPrice);
+    })
+    
+})
+
 
 
 
